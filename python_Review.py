87457f5613cdce563c39be8359ec0cd9f006dbc0
sivanuz23-meet <109904738+sivanuz23-meet@users.noreply.github.com>
2022-07-24 22:24:04 +0300

    Update python_Review.py

--- python_Review.py
+++ python_Review.py
@@ -1,13 +1,21 @@
 def create_youtube_video(title, description):
-	youtube_video = {"title" : title, "description" : description, "likes" : 0, "dislikes" : 0, "comments":{"username" : ""}}
+	youtube_video = {"title" : title, "description" : description, "likes" : 0, "dislikes" : 0, "comments":{}}
 	return youtube_video
 def like(video):
 	if "likes" in video:
 		video["likes"]+=1
 	return video
 
-def like(video):
-	if "likes" in video:
-		video["likes"]+=1
+def dislike(video):
+	if "dislikes" in video:
+		video["dislikes"]+=1
 	return video
 def add_comment(video, username, comment_text):
+	video["comments"][username] = comment_text
+	return video 
+
+for i in range(495):
+	like(create_youtube_video("hello world", "video about the world"))
+print(like)
+
+print(add_comment(create_youtube_video("hello world", "video about the world"), "Sivan", "That's a good video"))
