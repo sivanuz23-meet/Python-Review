1ccbd59f91419739df1a329de8132a22c8fcad13
sivanuz23-meet <109904738+sivanuz23-meet@users.noreply.github.com>
2022-07-25 18:43:58 +0300

    Update python_Review.py

--- python_Review.py
+++ python_Review.py
@@ -1,5 +1,5 @@
-def create_youtube_video(title, description):
-	youtube_video = {"title" : title, "description" : description, "likes" : 0, "dislikes" : 0, "comments":{}}
+def create_youtube_video(title, description, hashtag):
+	youtube_video = {"title" : title, "description" : description, "likes" : 0, "dislikes" : 0, "comments":{}, "hashtag": hashtag}
 	return youtube_video
 def like(video):
 	if "likes" in video:
@@ -14,9 +14,19 @@ def add_comment(video, username, comment_text):
 	video["comments"][username] = comment_text
 	return video
 
-youtube = create_youtube_video("hello world", "video about the world")
-for i in range(495):
-	like(youtube)
-print(like(youtube))
+def similarity_to_video(a,b):
+	counter = 0
+	for i in a:
+		if a[i] == b[i]:
+			counter+=1
+	percentage = counter/len(b)
+	print(str(percentage*100.0) + "%")
 
-print(add_comment(youtube, "Sivan", "That's a good video"))
+youtube1 = create_youtube_video("hello world", "video about the world", ["#great", "#good movie"])
+youtube2 = create_youtube_video("testing", "random description", ["#good", "#brilliant"])
+similarity_to_video(youtube1, youtube2)
+for i in range(495):
+	like(youtube1)
+print(like(youtube1))
+
+print(add_comment(youtube1, "Sivan", "That's a good video"))
